8094f2c47e336c88a17d988f25e7d733caad5c73
Gustavo Bertolino <gustavobertolino@gmail.com>
2020-04-26 20:54:18 -0300

    Add db migration integration. Add payment entity

--- backend/project.clj
+++ backend/project.clj
@@ -1,12 +1,26 @@
 (defproject backend "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "gool-pay: payment gateway in clojure"
+  :url "http://github.com/gustavobertolino/gool-pay"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
-  :plugins [[lein-ring "0.12.5"]]
+                 [ring/ring-defaults "0.3.2"]
+                 [migratus "1.2.8"]
+                 [mysql/mysql-connector-java "5.1.6"]]
+  :plugins [
+    [lein-ring "0.12.5"]
+    [migratus-lein "0.7.3"]]
   :ring {:handler backend.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+  :migratus {:store :database
+            :migration-dir "migrations"
+            :db {:classname "com.mysql.jdbc.Driver"
+                :subprotocol "mysql"
+                :subname "//localhost:3306/gool-pay"
+                :user "root"
+                :password "myuser"}}
+  :profiles {
+    :dev {
+      :dependencies [
+        [javax.servlet/servlet-api "2.5"]
+        [ring/ring-mock "0.3.2"]]}}
+)
---
+++ backend/resources/migrations/20200426171450-add-payment.up.sql
@@ -0,0 +1,12 @@
+BEGIN TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS payment(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    user_id INT NOT NULL,
+    store_id INT NOT NULL,
+    payment_method VARCHAR(50) NOT NULL, 
+    card_brand VARCHAR(50)
+);
+
+SELECT * FROM payment;
+COMMIT;
