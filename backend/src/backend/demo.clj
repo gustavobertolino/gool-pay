9420ff8a02131f2e656d3ae679f4a51fd47487cb
Gustavo Bertolino <gustavobertolino@gmail.com>
2020-04-27 20:05:33 -0300

    Add Canva lib to work with inline loading and running REPL

--- backend/src/backend/demo.clj
+++ backend/src/backend/demo.clj
@@ -1,6 +1,15 @@
 (ns backend.demo
-    (:require [clojure.string :as string]))
+    (:require [clojure.string :as string]
+              [clojure.tools.logging :as log]))
 
 (defn capitalizing [some_string] (string/capitalize some_string))
 
 (defn greet [name] (str "Hello, " name))
+
+(defn dividing [x y] (try
+                    (/ x y) ;; <-- dividing by zero works bad.
+                       (catch Exception e
+                         (log/error e "Dividing failed!")))
+)
+
+(dividing 10 0)
