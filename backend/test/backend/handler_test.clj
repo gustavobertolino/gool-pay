6bc07e90336a0a8421958e83579fbe4c77a26071
Gustavo <gustavobertolino@gmail.com>
2020-10-17 18:59:30 -0300

    Adjust indentation unit test for handling

--- backend/test/backend/handler_test.clj
+++ backend/test/backend/handler_test.clj
@@ -1,7 +1,7 @@
 (ns backend.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [backend.handler :refer :all]))
+            [backend.payment_handler :refer :all]))
 
 (deftest test-app
   (testing "main route"
